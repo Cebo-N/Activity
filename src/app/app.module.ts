2a4be9c03e467ba124a45a0d3d850e3369a910ad
Cebo N <cebonqweniso00@gmail.com>
2022-01-20 17:04:06 +0200

    initialize services and components files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressComponent } from './components/address/address.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { ActivityComponent } from './components/activity/activity.component';
+import { CompanionComponent } from './components/companion/companion.component';
+import { ActivityService } from './services/activity.service';
+import { CompanionService } from './services/companion.service';
+import { WeatherService } from './services/weather.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressComponent,
+    WeatherComponent,
+    ActivityComponent,
+    CompanionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ActivityService,
+    CompanionService,
+    WeatherService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
